8e77739c96d1baebda6d806deed9e576e513a6b2
Diva gupta <divagupta712@gmail.com>
2024-06-07 16:32:25 +0530

    updated user route

--- server/routes/user.Router.js
+++ server/routes/user.Router.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { upload } = require('../middlewares/multer.Middleware');
+const { registerUser } = require("../controllers/user.Controller")
+
+router.route("/register").post(upload.single('avatar'), registerUser);
+
 
 
 module.exports = router
